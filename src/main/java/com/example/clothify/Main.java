d465788b55af5d4c5881cf9aac49b9571932b5f9
SenithiGamage <gamagesenithi25@gmail.com>
2026-02-23 08:14:44 +0530

    initial commit

--- src/main/java/com/example/clothify/Main.java
+++ src/main/java/com/example/clothify/Main.java
@@ -1,4 +1,4 @@
-package edu.icet.ecom;
+package com.example.clothify;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/com/example/clothify/Starter.java
+++ src/main/java/com/example/clothify/Starter.java
@@ -1,4 +1,4 @@
-package edu.icet.ecom;
+package com.example.clothify;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
